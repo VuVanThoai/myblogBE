32f0f641ef9a033eb535e626b38f6b0c76e14585
thoaidz <zzaazzbadao@gmail.com>
2021-09-06 14:24:31 +0700

    test

--- Blog/src/main/java/com/thoaidz/blog/controller/ArticleController.java
+++ Blog/src/main/java/com/thoaidz/blog/controller/ArticleController.java
@@ -79,7 +79,6 @@ public class ArticleController {
 		if (newArticle != null) {
 			return new ResponseEntity<Article>(newArticle, HttpStatus.OK);
 		} else {
-			System.err.println("---error---------");
 			return new ResponseEntity<Article>(HttpStatus.BAD_GATEWAY);
 		}
 	}
@@ -94,5 +93,4 @@ public class ArticleController {
 			return new ResponseEntity<Article>(HttpStatus.BAD_GATEWAY);
 		}
 	}
-
 }
