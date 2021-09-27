12f3bcc57ebe2676fea14a9c1e4578762170d794
thoaidz <zzaazzbadao@gmail.com>
2021-09-27 08:21:06 +0700

    no msg

--- src/main/java/com/thoaidz/LearnEnglishApplication.java
+++ src/main/java/com/thoaidz/LearnEnglishApplication.java
@@ -2,20 +2,12 @@ package com.thoaidz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearnEnglishApplication extends SpringBootServletInitializer {
+public class LearnEnglishApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnEnglishApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(LearnEnglishApplication.class);
 	}
 
 }
